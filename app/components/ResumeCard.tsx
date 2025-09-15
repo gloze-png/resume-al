782abe5f7eeb21c5bcb01154fbc51370795f5f58
Glory <gloryope247@gmail.com>
2025-09-15 13:00:36 +0100

    homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router'
+import ScoreCircle from './ScoreCircle'
 
-const ResumeCard = () => {
+const ResumeCard = ({ resume }: { resume: Resume }) => {
   return (
-    <div>ResumeCard</div>
+    <Link to={`/resume/${resume.id}`} 
+    className='resume-card animate-in fade-in duration-1000'>
+      <div className="resume-card-header">
+      <div className='flex flex-col gap-2'>
+        <h2 className='!text-black font-bold break-words'>{resume.companyName}</h2>
+        <h3 className='text-lg break-words text-gray-500'>{resume.jobTitle}</h3>
+      </div>
+      <div className='flex-shrink-0'>
+        <ScoreCircle score={resume.feedback?.overallScore ?? 0} />
+      </div>
+      </div>
+      <div className='gradient-border animate-in fade-in duration-1000'>
+        <div className='w-full h-full'>
+       <img src={resume.imagePath}
+          alt ="resume"
+          className='w-full h-[350px] max-sm:h-[200px] object-cover object-top' 
+          />
+        </div>
+      </div>
+      </Link>
   )
 }
 
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants"
+import ResumeCard from "~/components/ResumeCard";
 
 // const resumes = [
 //   { jobTitle: "Frontend Engineer" },
@@ -25,11 +26,13 @@ export default function Home() {
       <h1>Track Your Application and Resume Ratings</h1>
       <h2>Get Notified When Your Resume is Viewed</h2>
     </div>
-  </section>
- {resumes.map((resume) => (
-    <div>
-      <h1>{resume.jobTitle}</h1>
+{resumes.length> 0 && (
+  <div className="resumes-section">
+    {resumes.map((resume) => (
+      <ResumeCard key={resume.id} resume={resume} />
+    ))}
     </div>
-  ))}
+    )}
+    </section>
  </main>
 }
